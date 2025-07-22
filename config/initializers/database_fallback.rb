523bfd4f73514213a1cf7dac0fd63c28928a24ce
Hamza Khalid <hamzakhalidovic@gmail.com>
2025-07-22 11:05:35 +0100

    Remove problematic database_fallback.rb causing startup crash

--- config/initializers/database_fallback.rb
+++
@@ -1,9 +0,0 @@
-# config/initializers/database_fallback.rb
-begin
-  # Test database connection at startup
-  ActiveRecord::Base.connection.execute("SELECT 1")
-  Rails.logger.info "Database connection successful"
-rescue => e
-  Rails.logger.error "Database connection failed: #{e.message}"
-  # Continue anyway to allow the app to start
-end
